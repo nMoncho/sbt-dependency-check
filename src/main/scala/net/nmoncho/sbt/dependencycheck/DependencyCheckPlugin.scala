1103cba999c2897e4817f52bb813bac94fab12bf
Gustavo De Micheli <gustavo.de.micheli@lunatech.com>
2025-03-17 09:41:26 +0100

    chore: Add License Header and style aliases

--- src/main/scala/net/nmoncho/sbt/dependencycheck/DependencyCheckPlugin.scala
+++ src/main/scala/net/nmoncho/sbt/dependencycheck/DependencyCheckPlugin.scala
@@ -1,11 +1,37 @@
+/*
+ * Copyright (c) 2025 the original author or authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package net.nmoncho.sbt.dependencycheck
 
-import net.nmoncho.sbt.dependencycheck.settings.*
-import net.nmoncho.sbt.dependencycheck.tasks.*
+import net.nmoncho.sbt.dependencycheck.settings._
+import net.nmoncho.sbt.dependencycheck.tasks._
 import org.owasp.dependencycheck.reporting.ReportGenerator.Format
 import org.owasp.dependencycheck.utils.Settings
-import sbt.Keys.*
-import sbt.{ Def, * }
+import sbt.AutoPlugin
+import sbt.Def
+import sbt.File
+import sbt.Keys._
+import sbt.PluginTrigger
+import sbt.Task
+import sbt.io.syntax._
 import sbt.plugins.JvmPlugin
 
 object DependencyCheckPlugin extends AutoPlugin {
