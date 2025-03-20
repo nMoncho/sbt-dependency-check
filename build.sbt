e9231c06280020fa515591fb8e96d525d8bbd974
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2025-03-20 16:25:03 +0100

    chore: Add License Header and style aliases

--- build.sbt
+++ build.sbt
@@ -2,22 +2,52 @@ import Dependencies.*
 
 ThisBuild / organization := "net.nmoncho"
 
+addCommandAlias(
+  "testCoverage",
+  "; clean ; coverage; test; coverageAggregate; coverageReport; coverageOff"
+)
+
+addCommandAlias(
+  "styleFix",
+  "; scalafmtSbt; +scalafmtAll; +headerCreateAll; scalafixAll"
+)
+
+addCommandAlias(
+  "styleCheck",
+  "; +scalafmtCheckAll; +headerCheckAll; scalafixAll --check"
+)
+
 lazy val root = (project in file("."))
   .enablePlugins(SbtPlugin)
   .settings(
     name := "sbt-dependency-check",
+    startYear := Some(2025),
+    homepage := Some(url("https://github.com/nMoncho/sbt-dependency-check")),
+    licenses := Seq("MIT License" -> new URL("http://opensource.org/licenses/MIT")),
+    headerLicense := Some(HeaderLicense.MIT("2025", "the original author or authors")),
+    sonatypeCredentialHost := "s01.oss.sonatype.org",
+    sonatypeRepository := "https://s01.oss.sonatype.org/service/local",
+    semanticdbEnabled := true,
+    semanticdbVersion := scalafixSemanticdb.revision,
+    scalacOptions := (Opts.compile.encoding("UTF-8") :+
+      Opts.compile.deprecation :+
+      Opts.compile.unchecked :+
+      "-feature" :+
+      "-Ywarn-unused" :+
+      "-Xlog-implicits"),
     libraryDependencies ++= Seq(
       dependencyCheck,
       munit % Test
     ),
     pluginCrossBuild / sbtVersion := {
       scalaBinaryVersion.value match {
-        case "2.12" => "1.9.0" // set minimum sbt version so we have `sbtPluginPublishLegacyMavenStyle`
+        case "2.12" =>
+          "1.9.0" // set minimum sbt version so we have `sbtPluginPublishLegacyMavenStyle`
       }
     },
     scriptedLaunchOpts := {
       scriptedLaunchOpts.value ++
-        Seq("-Xmx1024M", "-Dplugin.version=" + version.value)
+      Seq("-Xmx1024M", "-Dplugin.version=" + version.value)
     },
     scriptedBufferLog := false
   )
--- src/main/scala/net/nmoncho/sbt/dependencycheck/tasks/package.scala
+++ src/main/scala/net/nmoncho/sbt/dependencycheck/tasks/package.scala
@@ -1,23 +1,55 @@
+/*
+ * Copyright (c) 2025 the original author or authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package net.nmoncho.sbt.dependencycheck
 
-import net.nmoncho.sbt.dependencycheck.Keys.{ dependencyCheckScopes, dependencyCheckSkip }
+import java.io.PrintWriter
+import java.io.StringWriter
+
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+import scala.util.Using
+import scala.util.control.NonFatal
+
+import net.nmoncho.sbt.dependencycheck.Keys.dependencyCheckScopes
+import net.nmoncho.sbt.dependencycheck.Keys.dependencyCheckSkip
 import net.nmoncho.sbt.dependencycheck.settings.ScopesSettings
 import org.owasp.dependencycheck.Engine
 import org.owasp.dependencycheck.agent.DependencyCheckScanAgent
 import org.owasp.dependencycheck.data.nexus.MavenArtifact
-import org.owasp.dependencycheck.dependency.naming.{ GenericIdentifier, Identifier, PurlIdentifier }
-import org.owasp.dependencycheck.dependency.{ Confidence, Dependency, EvidenceType }
+import org.owasp.dependencycheck.dependency.Confidence
+import org.owasp.dependencycheck.dependency.Dependency
+import org.owasp.dependencycheck.dependency.EvidenceType
+import org.owasp.dependencycheck.dependency.naming.GenericIdentifier
+import org.owasp.dependencycheck.dependency.naming.Identifier
+import org.owasp.dependencycheck.dependency.naming.PurlIdentifier
 import org.owasp.dependencycheck.exception.ExceptionCollection
 import org.owasp.dependencycheck.reporting.ReportGenerator.Format
+import org.owasp.dependencycheck.utils.Settings
 import org.owasp.dependencycheck.utils.Settings.KEYS.APPLICATION_NAME
-import org.owasp.dependencycheck.utils.{ Settings, SeverityUtil }
-import sbt.Keys.*
+import org.owasp.dependencycheck.utils.SeverityUtil
+import sbt.Keys._
 import sbt.plugins.JvmPlugin
-import sbt.{ Keys as SbtKeys, * }
-
-import java.io.{ PrintWriter, StringWriter }
-import scala.util.control.NonFatal
-import scala.util.{ Failure, Success, Try, Using }
+import sbt.{ Keys => SbtKeys, _ }
 
 package object tasks {
 
