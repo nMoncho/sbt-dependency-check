a71c2030483a17a60bb39ae6a414014529fcf08d
Scala Steward <scala_steward@virtuslab.com>
2025-03-21 18:46:59 +0000

    Update munit to 1.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt.*
 
 object Dependencies {
   lazy val dependencyCheck = "org.owasp"      % "dependency-check-core" % "12.1.0"
-  lazy val munit           = "org.scalameta" %% "munit"                 % "1.0.2"
+  lazy val munit           = "org.scalameta" %% "munit"                 % "1.0.4"
 }
