43c63c9abc5e12cacd9cb1309f48aece8252fd30
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-04-10 19:18:44 +0200

    Update dependency-check-core to 12.1.1 (#11)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object Dependencies {
-  lazy val dependencyCheck = "org.owasp"                % "dependency-check-core" % "12.1.0"
+  lazy val dependencyCheck = "org.owasp"                % "dependency-check-core" % "12.1.1"
   lazy val munit           = "org.scalameta"           %% "munit"                 % "1.1.0"
   lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"      % "1.1.0"
   lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl"     % "2.24.3"
