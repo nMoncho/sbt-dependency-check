297b1d766450a9610e18bbd9a58f478c7036eecc
Scala Steward <scala_steward@virtuslab.com>
2025-06-07 16:32:41 +0000

    Update dependency-check-core to 12.1.2

--- src/test/scala/net/nmoncho/sbt/dependencycheck/tasks/ListSettingsSuite.scala
+++ src/test/scala/net/nmoncho/sbt/dependencycheck/tasks/ListSettingsSuite.scala
@@ -183,7 +183,7 @@ class ListSettingsSuite extends munit.FunSuite {
       |	nvd.api.results.per.page: null
       |	odc.analysis.timeout: 180
       |	odc.application.name: Dependency-Check Core
-      |	odc.application.version: 12.1.1
+      |	odc.application.version: 12.1.2
       |	odc.autoupdate: true
       |	odc.ecosystem.maxquerylimit.: null
       |	odc.ecosystem.maxquerylimit.default: 100
