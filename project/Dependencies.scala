e2a726908cb9fae5b2f86f9908b36fcb891115ec
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-03 13:07:54 +0200

    chore(deps): update munit from 1.3.1 to 1.3.2 (#101)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Dependencies {
   lazy val dependencyCheck = "org.owasp"                % "dependency-check-core" % "12.2.2"
-  lazy val munit           = "org.scalameta"           %% "munit"                 % "1.3.1"
+  lazy val munit           = "org.scalameta"           %% "munit"                 % "1.3.2"
   lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"      % "1.3.0"
   lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl"     % "2.26.0"
   lazy val mockito         = "org.mockito"              % "mockito-core"          % "5.23.0"
