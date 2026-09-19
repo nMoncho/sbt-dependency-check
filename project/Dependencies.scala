da49acddf710a7a11c53bce88ee254c5832917e3
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2026-09-19 21:34:04 +0200

    chore(deps): update `munit-scalacheck`

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
   lazy val dependencyCheck = "org.owasp"                % "dependency-check-core" % "13.0.0"
   lazy val munit           = "org.scalameta"           %% "munit"                 % "1.3.5"
-  lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"      % "1.3.0"
+  lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"      % "1.3.1"
   lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl"     % "2.26.1"
   lazy val mockito         = "org.mockito"              % "mockito-core"          % "5.23.0"
 }
